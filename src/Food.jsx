79d946d1d4fdca0ea70e83c37ffd1034bbf4aaee
BacaV <nikolajevtic3405@gmail.com>
2024-06-19 19:27:56 +0200

    Array state change

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 
 function Food(){
 
-    const food1 = 'Orange';
-    const food2 = 'Banana';
+    const [foods, setFoods] = useState(['Orange','Banana','Apple'])
+
+    function handleAddFood(){
+
+        const newFood = document.getElementById("foodInput").value;
+        document.getElementById("foodInput").value = "";
+
+        setFoods([...foods, newFood]);
+    }
+
+    function handleRemoveFood(index){
+
+        setFoods(foods.filter((_, i) => i !== index));
+    }
+
 
 
     return(
-
+        <>
+        <h2>List of food</h2>
         <ul>
-            <li>Apple</li>
-            <li>{food1}</li>
-            <li>{food2}</li>
+            {foods.map((food, index) => <li onClick={() => handleRemoveFood(index)} key={index}>{food}</li>)}
         </ul>
+        <input type="text" id="foodInput" placeholder="Enter food"/>
+        <button onClick={handleAddFood}>Add food</button>
+        </>
     );
 
 }
