338ac6af88d3844ba02c408dac1598d5d0fd740b
BacaV <nikolajevtic3405@gmail.com>
2024-06-24 18:49:25 +0200

    Todo start

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,8 +1,53 @@
+import { useState } from "react";
+
 function ToDoList(){
 
     return( 
-        <h2>Naslov</h2>
+        <>
+        <h2>To do list</h2>
+        <InputElement />
+        </>
+        
+
     );
 }
+
+function InputElement(){
+
+    const [itemList, setItemList] = useState([]);
+    const [item, setItem] = useState("");
+
+    function handleListChange(e){
+        setItem(e.target.value);
+    }
+
+    function handleListAdd(){
+        setItemList(item);
+    }
+
+    return(
+        <>
+        <div>
+            <input type="text" value={item} id="listInput" onChange={handleListChange} placeholder="Enter list item" />
+            <button onClick={handleListAdd}></button>
+        </div>
+        <ItemElement value={itemList[0]} />
+        </>
+    )
+}
+
+function ItemElement(props){
+
+    const itemValue = props.value;
+
+    return(
+        <>
+        <div>
+            <p>{itemValue}</p>
+        </div>
+        </>
+    )
+}
+
 
 export default ToDoList;
